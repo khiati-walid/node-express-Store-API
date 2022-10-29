7d63442c799547a7cb0404e80ca2166809db033f
khiati-walid <khiatikhaledwalid@gmail.com>
2022-10-29 17:17:32 +0100

    feat: connection to db added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const connectDB = require("./db/connect");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -22,6 +24,7 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     // connect DB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server listening in ${port} ...`));
   } catch (error) {
     console.log(error);
