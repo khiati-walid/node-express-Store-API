c49c23e9f5e5983601020743591fbbb7baf7efd3
khiati-walid <khiatikhaledwalid@gmail.com>
2022-11-01 16:45:46 +0100

    feat: populate database, filtering added

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,27 @@
+const Product = require("../models/Product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "All products Static" });
+  const products = await Product.find();
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "All products" });
+  const { featured, name, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
